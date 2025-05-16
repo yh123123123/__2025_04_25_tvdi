5be6656a2acb7c361dad4c4acc8fe11b182b6959
yh123123123 <yunghsu123@yahoo.com.tw>
2025-05-16 13:38:24 +0800

    20250516

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello, World!</h1><p>01頁</p>"
+    return render_template("index.html")
 
 @app.route("/user")
 def user():
