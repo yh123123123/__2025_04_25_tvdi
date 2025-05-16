2e8b7150911332e2e672ede46c33315e1abcea7d
yh123123123 <yunghsu123@yahoo.com.tw>
2025-05-16 10:47:48 +0800

    20250516

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,1 +1,16 @@
-print("Hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello, World!</h1><p>01頁</p>"
+
+@app.route("/user")
+def user():
+    return "<h1>Hello, World!</h1><p>02頁</p>"
+
+@app.route("/product")
+def userproduct():
+    return "<h1>Hello, World!</h1><p>03頁</p>"
+
