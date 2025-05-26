5f91bff099b9e30a0cfcb00eb2a03e071f274154
yh123123123 <yunghsu123@yahoo.com.tw>
2025-05-26 09:04:33 +0800

    20250526

---
+++ lesson6/index.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/traffic")
+def traffic():
+    return render_template("traffic.html")
+
+@app.route("/user")
+def user():
+    return "<h1>Hello, World!</h1><p>02頁</p>"
+
+@app.route("/product")
+def userproduct():
+    return "<h1>Hello, World!</h1><p>03頁</p>"
+
